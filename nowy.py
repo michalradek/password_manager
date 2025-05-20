4a32d1ede90f1ade50ad313ae893e6d843db30e4
michal.radek <michal.radek.nowakowski@gmail.com>
2025-05-20 03:49:36 +0200

    WIP

--- nowy.py
+++ nowy.py
@@ -7,13 +7,16 @@ from password_manager_backend import login, logout, add_password, print_all
 
 root = tk.Tk()
 root.title("Password manager")
-root.geometry("600x500")
+root.geometry("1200x700")
+root.resizable(False, False)
+
 
 def on_click_login():
-    login(frame_login, main_frame, entry_master)
+    login(login_frame, main_frame, master_entry)
+    on_click_refresh()
 
 def on_click_logout():
-    logout(main_frame, frame_login, entry_master)
+    logout(main_frame, login_frame, master_entry)
 
 def on_click_add():
     add_password(entry_site, entry_username, entry_password)
@@ -23,21 +26,39 @@ def on_click_refresh():
         tree.delete(i)
     for line in print_all():
         tree.insert("", tk.END, values=(line[1], line[2], line[3]))
-    
+
+def on_tree_select(event):
+    selected_item = tree.focus()
+    if selected_item:
+        values = tree.item(selected_item, 'values')
+        details_service_var.set(values[0])
+        
+
+def on_right_click(event):
+    selected_item = tree.identify_row(event.y)
+    if selected_item:
+        tree.selection_set(selected_item)
+        popup_menu.post(event.x_root, event.y_root)
+
+def delete_selected_item():
+    selected_item = tree.selection()
+    if selected_item:
+        tree.delete(selected_item[0])
+
 # Ekran logowanie
 
-frame_login = tk.Frame(root)
-label_login = tk.Label(frame_login, text="Enter master password: ")
-entry_master = tk.Entry(frame_login, show="*")
-button_login = tk.Button(frame_login, text="Zaloguj", command=on_click_login)
+login_frame = tk.Frame(root)
+login_label = tk.Label(login_frame, text="Enter master password: ")
+master_entry = tk.Entry(login_frame, show="*")
+login_button = tk.Button(login_frame, text="Zaloguj", command=on_click_login)
 
-label_login.pack(pady=10, fill="x")
-entry_master.pack(pady=5, fill="x")
-button_login.pack(pady=10, fill="x")
-frame_login.pack(expand=True)
-
+login_label.pack(pady=10, fill="x")
+master_entry.pack(pady=5, fill="x")
+login_button.pack(pady=10, fill="x")
+login_frame.pack(expand=True)
 
 # main frame
+
 main_frame = tk.Frame(root)
 
 top_button_frame = tk.Frame(main_frame)
@@ -48,20 +69,50 @@ logout_button.grid(row=0, column=0, sticky="w", padx=10, pady=5)
 refresh_button.grid(row=0, column=1, sticky="e", padx=10, pady=5)
 top_button_frame.grid_columnconfigure(0, weight=1)
 top_button_frame.grid_columnconfigure(1, weight=1)
+title_label = tk.Label(main_frame, text="Saved passoword's")
+title_label.pack()
 
-label_title = tk.Label(main_frame, text="Saved passoword's")
-label_title.pack()
-tree = ttk.Treeview(main_frame, columns=("service", "login", "password"), show="headings")
+content_frame = tk.Frame(main_frame)
+content_frame.pack(fill="both", expand=True, padx=10, pady=10)
+tree = ttk.Treeview(content_frame, columns=("service", "login", "password"), show="headings")
 tree.heading("service", text="service")
 tree.heading("login", text="login")
 tree.heading("password", text="password")
-tree.column("service", width=150)
-tree.column("login", width=150)
-tree.column("password", width=150)
-tree.pack(expand=True, fill="both", padx=10, pady=10)
+tree.column("service", width=250)
+tree.column("login", width=250)
+tree.column("password", width=250)
+tree.bind("<<TreeviewSelect>>", on_tree_select)
+tree.bind("<Button-3>", on_right_click)
+
+details_frame = tk.Frame(content_frame, borderwidth=2)
+details_frame.grid(row=0, column=1, sticky="nsew", padx=(10,0))
+details_label = tk.Label(details_frame, text="Details")
 
 
-# okno dodawania
+details_service_frame = tk.Frame(details_frame)
+details_service_label = tk.Label(details_service_frame, text="Service: ")
+details_service_var = tk.StringVar()
+details_service_entry = tk.Entry(details_service_frame, justify="left", textvariable=details_service_var, state="readonly", width=40)
+
+tree.grid(row=0, column=0, sticky="nsew")
+details_label.pack(pady=(10,20))
+details_service_frame.pack(fill="both", padx=10, pady=10)
+details_service_label.grid(row=0, column=0, sticky="w", padx=(10,0))
+details_service_entry.grid(row=0, column=1, sticky="w", padx=(10,0))
+
+content_frame.columnconfigure(0, weight=0)
+content_frame.columnconfigure(1, weight=0)
+content_frame.rowconfigure(0, weight=1)
+details_service_frame.columnconfigure(0, weight=1)
+details_service_frame.columnconfigure(1, weight=1)
+details_service_frame.rowconfigure(0, weight=1)
+
+# right click popup_menu
+
+popup_menu = tk.Menu(root, tearoff=0)
+popup_menu.add_command(label="delete", command=delete_selected_item)
+
+# entry frame
 
 entry_site = tk.Entry(main_frame)
 entry_site.insert(0, "serwis")
@@ -70,7 +121,6 @@ entry_username.insert(0, "Login")
 entry_password = tk.Entry(main_frame, show="*")
 entry_password.insert(0, "Password")
 entry_button = tk.Button(main_frame, text="Add password", command=on_click_add)
-
 
 entry_site.pack(pady=10, fill="x")
 entry_username.pack(pady=10, fill="x")
